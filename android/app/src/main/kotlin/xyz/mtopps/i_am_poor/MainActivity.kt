52f2cb8c71462c32689977322782331654266bad
Matthew Topping <matthewatopping@gmail.com>
2022-10-01 15:42:02 +1300

    project from 2021 flutter bootcamp

---
+++ android/app/src/main/kotlin/xyz/mtopps/i_am_poor/MainActivity.kt
@@ -0,0 +1,6 @@
+package xyz.mtopps.i_am_poor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.brown,
+          title: const Center(child: Text('Im hella poor')),
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/download.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
